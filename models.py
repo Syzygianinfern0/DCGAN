e9ab8cd442794c2dfa52d01f7609324b59cd0725
Syzygianinfern0 <spsharan2000@gmail.com>
2019-12-15 18:38:57 +0530

    Discriminator Model is Up

--- models.py
+++ models.py
@@ -20,13 +20,13 @@ class Generator(nn.Module):
 
         blocks = [self.trans_conv_block(in_c, out_c, stride, padding)
                   for in_c, out_c, stride, padding in
-                  zip(channels[:-1], channels[1:], strides, paddings)]
+                  zip(channels[:-2], channels[1:], strides, paddings)]
         blocks.append(self.trans_conv_block(channels[-2], channels[-1], strides[-1], paddings[-1], act='tanh'))
 
         self.generator = nn.Sequential(*list(blocks))
 
     def forward(self, x):
-        return self.main(x)
+        return self.generator(x)
 
     @staticmethod
     def trans_conv_block(in_c, out_c, stride, padding, act='relu', *args, **kwargs):
@@ -47,9 +47,69 @@ class Generator(nn.Module):
             )
 
 
+class Discriminator(nn.Module):
+    def __init__(self):
+        super(Discriminator, self).__init__()
+
+        strides = [2, 2, 2, 2, 1]
+        paddings = [1, 1, 1, 1, 0]
+        channels = [nc,
+                    ndf,
+                    ndf * 2,
+                    ndf * 4,
+                    ndf * 8,
+                    1]
+        bns = [False, True, True, True, True]
+
+        blocks = [self.trans_conv_block(in_c, out_c, stride, padding, bn=bn)
+                  for in_c, out_c, stride, padding, bn in
+                  zip(channels[:-2], channels[1:], strides, paddings, bns)]
+
+        blocks.append(self.trans_conv_block(channels[-2], channels[-1], strides[-1],
+                                            paddings[-1], act='sigmoid', bn=bns[-1]))
+
+        self.generator = nn.Sequential(*list(blocks))
+
+    def forward(self, x):
+        return self.disciminator(x)
+
+    @staticmethod
+    def trans_conv_block(in_c, out_c, stride, padding, act='relu', bn=True, *args, **kwargs):
+        if act.lower() == 'relu':
+            if bn is True:
+                return nn.Sequential(
+                    nn.Conv2d(in_c, out_c, kernel_size=4,
+                              stride=stride, padding=padding,
+                              bias=False, *args, **kwargs),
+                    nn.BatchNorm2d(out_c),
+                    nn.LeakyReLU()
+                )
+            else:
+                return nn.Sequential(
+                    nn.Conv2d(in_c, out_c, kernel_size=4,
+                              stride=stride, padding=padding,
+                              bias=False, *args, **kwargs),
+                    nn.LeakyReLU()
+                )
+        elif act.lower() == 'sigmoid':
+            return nn.Sequential(
+                nn.ConvTranspose2d(in_c, out_c, kernel_size=4,
+                                   stride=stride, padding=padding,
+                                   bias=False, *args, **kwargs),
+                nn.Sigmoid()
+            )
+
+
 netG: nn.Module = Generator()
 netG.to(device)
 if device.type == 'cuda':
     netG = nn.DataParallel(netG, [0])
 netG.apply(weights_init)
 print(netG)
+
+netD: nn.Module = Discriminator()
+netD.to(device)
+if device.type == 'cuda':
+    netG = nn.DataParallel(netD, [0])
+netD.apply(weights_init)
+print(netD)
